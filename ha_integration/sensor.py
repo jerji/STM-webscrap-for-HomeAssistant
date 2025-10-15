025750987b4ae5c0121774bef9929ea09068c35b
Angel Ouellet <angel@jouellet.net>
2025-10-15 19:42:32 -0400

    Initial commit of integration

---
+++ ha_integration/sensor.py
@@ -0,0 +1,46 @@
+import asyncio
+import logging
+from homeassistant.helpers.entity import Entity
+from .stm_api import fetch_metro_status
+
+_LOGGER = logging.getLogger(__name__)
+
+LINE_SENSORS = {
+    "Line 1 - Green": "stm_metro_line_1_status",
+    "Line 2 - Orange": "stm_metro_line_2_status",
+    "Line 5 - Blue": "stm_metro_line_5_status",
+    "Line 4 - Yellow": "stm_metro_line_4_status",
+}
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the STM Metro Status sensors."""
+    statuses = await hass.async_add_executor_job(fetch_metro_status)
+    sensors = [
+        STMMetroSensor(line, statuses.get(line, "Unknown"))
+        for line in LINE_SENSORS
+    ]
+    async_add_entities(sensors, True)
+
+class STMMetroSensor(Entity):
+    """Representation of an STM Metro Line Status sensor."""
+
+    def __init__(self, line_name, status):
+        self._line_name = line_name
+        self._state = status
+        self._attr_unique_id = LINE_SENSORS[line_name]
+        self._attr_name = f"STM {line_name}"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Return an icon representing the sensor."""
+        return "mdi:subway"
+
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        statuses = await asyncio.get_event_loop().run_in_executor(None, fetch_metro_status)
+        self._state = statuses.get(self._line_name, "Unknown")
