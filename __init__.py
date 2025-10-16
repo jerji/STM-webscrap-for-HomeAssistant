eac581ad61a27a364cab4add8607e48529b3cee8
Angel Ouellet <angel@jouellet.net>
2025-10-15 21:15:19 -0400

    Add missing parts of init.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,19 @@
-from homeassistant.helpers.discovery import async_load_platform
+# custom_components/stm_metro_status/__init__.py
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 
 DOMAIN = "stm_metro_status"
 
-async def async_setup(hass, config):
+async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up the STM Metro Status integration."""
-    hass.data[DOMAIN] = {}
-    hass.async_create_task(
-        async_load_platform(hass, "sensor", DOMAIN, {}, config)
-    )
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry):
+    """Set up STM Metro Status from a config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry):
+    """Unload STM Metro Status config entry."""
+    await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     return True
